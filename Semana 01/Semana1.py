d12305afb285c3f83e22cf9fa4ff28846bde710b
BonoCoder <adamspaiva@gmail.com>
2023-01-23 19:21:11 -0300

    s1 e s2

---
+++ Semana 01/Semana1.py
@@ -0,0 +1,17 @@
+
+# Simple way to start a python program
+print("hello world!\n")
+
+# So functions is some word that are reserved to perform a action and keyword are used to construct instructions!
+
+# A diferent way using a variable
+name = "Adam"
+print("Welcome to the course " + name)
+
+# Python can be a calculator
+print("Adam your age is ")
+print(20+8)
+#Some usefull tips about math problems
+# a ** b    Is the same as a power(^) b
+# a // b    Gives the integer part of the divison of a by b
+# a % b     Gives the remainder part of the division of a by b
--- Semana 01/TestePython.py
+++
@@ -1,1 +0,0 @@
-print("Vamos que vamoos, testando agora")
