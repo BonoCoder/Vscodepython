39ad7eccbd1a0c738654d030eb852705af0f0aa9
BonoCoder <adamspaiva@gmail.com>
2023-02-07 16:36:22 -0300

    commit test

--- Semana 04/Semana4.py
+++ Semana 04/Semana4.py
@@ -1,7 +1,9 @@
-def factorial(n):
-  if n < 2:
+def sum_positive_numbers(n):
+  if n == 1:
     return 1
-  return n * factorial(n-1)
+  return n + sum_positive_numbers(n-1)
 
 
-print(factorial(5))
+print(sum_positive_numbers(5)) # Should be 15
+
+
